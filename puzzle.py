0cb425fc27330f68b921b543b668605a966586c8
Ian Kim <iank0426@gmail.com>
2025-11-22 14:35:18 -0500

    finished

--- puzzle.py
+++ puzzle.py
@@ -37,7 +37,30 @@ class BlockPuzzleSearchSpace(SearchSpace):
         bool
             True iff the state is a final state
         """
-        raise NotImplementedError('Implement me!')
+        x = 0
+        y = 0
+        z = 0
+        coordinates = [(x, y, z)]
+        map = { "E": (1, 0, 0), "W": (-1, 0, 0), "N": (0, 1, 0), "S": (0, -1, 0), "U": (0, 0, 1), "D": (0, 0, -1) }
+
+        for move in state:
+            dx, dy, dz = map[move]
+            x, y, z = x + dx, y + dy, z + dz
+            coordinates.append((x, y, z))
+
+        if len(coordinates) != len(self.intervals) + 1:  # making sure length matches
+            return False
+
+        if len(coordinates) != len(set(coordinates)):  # making sure no coords revisit
+            return False
+
+        for (x, y, z) in coordinates:  # making sure within cube bounds
+            if not (0 <= x < self.cube_width and 0 <= y < self.cube_width and 0 <= z <
+                    self.cube_width):
+                return False
+
+        return True
+
 
     def get_successors(self, state):
         """Determines the possible successors of a state.
@@ -64,7 +87,58 @@ class BlockPuzzleSearchSpace(SearchSpace):
         list[tuple[str]]
             The list of valid successor states.
         """
-        # isPivot = len(state) ==         
+        if not isinstance(state, tuple):  # check for all tuples (otherwise error happens)
+            state = tuple(state)
+
+        map = { "E": (1, 0, 0), "W": (-1, 0, 0), "N": (0, 1, 0), "S": (0, -1, 0), "U": (0, 0, 1), "D": (0, 0, -1) }
+
+        directions = {
+            "E": ["U", "N", "S", "D"], "W": ["U", "N", "S", "D"],
+            "N": ["E", "W", "U", "D"], "S": ["E", "W", "U", "D"],
+            "U": ["E", "W", "N", "S"], "D": ["E", "W", "N", "S"],
+        }
+
+        current_structure = []
+        prev_direction = state[0]
+        count = 1
+        for i in range(1, len(state)):
+            if state[i] == prev_direction:
+                count += 1
+            else:
+                current_structure.append(count)
+                prev_direction = state[i]
+                count = 1
+        current_structure.append(count)
+        curr_piece_len = current_structure[-1]
+
+        if curr_piece_len < self.intervals[len(current_structure)-1]:  # check point
+            return [state + (state[-1],)]  # straight
+
+        new_successors = []
+        for turn in directions[state[-1]]:
+            candidate = state + (turn,)
+            x, y, z = 0, 0, 0
+            coordinates = [(x, y, z)]
+            for move in candidate:
+                dx, dy, dz = map[move]
+                x, y, z = x + dx, y + dy, z + dz
+                coordinates.append((x, y, z))
+
+            if len(coordinates) != len(set(coordinates)):
+                continue
+
+            x_vals = [p[0] for p in coordinates]
+            y_vals = [p[1] for p in coordinates]
+            z_vals = [p[2] for p in coordinates]
+
+            # make sure within cube bounds
+            if (max(x_vals) - min(x_vals) < self.cube_width and max(y_vals) - min(
+                    y_vals) < self.cube_width and max(z_vals) - min(z_vals) <
+                    self.cube_width):
+                new_successors.append(candidate)
+
+        return new_successors
+ 
        
 
 def construct_search_space_for_2x2x2_puzzle():
